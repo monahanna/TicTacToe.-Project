59c58d51fb549ab25ef96c4426cebe7aa01bfa96
monahanna <142627246+monahanna@users.noreply.github.com>
2024-04-22 21:35:06 -0400

    Update Tic-Tac-Toe Gui.py
    fixing the function to check if a position on the board is available

--- Tic-Tac-Toe Gui.py
+++ Tic-Tac-Toe Gui.py
@@ -26,6 +26,7 @@ def CheckPosition(x):
         return False     
         
 # Function to draw the game board using the selected theme
+def DrawBoard(theme):  
     # Apply colors from the selected theme
     border_color = theme['border']
     empty_color = theme['empty']
